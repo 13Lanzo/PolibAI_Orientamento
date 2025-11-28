77b355ec0474331fef65bce29fe75c27730e4e32
pierpaolo <pier.paolo.cann.one90@gmail.com>
2025-11-28 09:32:10 +0100

    feat: Implement Hero component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 import { Header } from './components/header/header';
 import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Navbar],
+  imports: [RouterOutlet, Header, Navbar, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
